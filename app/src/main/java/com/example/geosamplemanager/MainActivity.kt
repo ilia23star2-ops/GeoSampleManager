2c3329722173b58bbe850b90cbaedb1ab2172997
ilia23star2-ops <ilia23star2@gmail.com>
2026-09-10 15:45:25 +0500

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/geosamplemanager/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.geosamplemanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.geosamplemanager.ui.navigation.AppScaffold
+import com.example.geosamplemanager.ui.theme.GeoSampleManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GeoSampleManagerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppScaffold()
+                }
+            }
+        }
+    }
+}
