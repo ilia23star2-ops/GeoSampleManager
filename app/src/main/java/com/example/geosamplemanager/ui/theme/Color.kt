f6ce811a2d635117eda8d6dfeb005fd981c91ce7
ilia23star2-ops <ilia23star2@gmail.com>
2026-09-10 15:46:10 +0500

    Create Color.kt

---
+++ app/src/main/java/com/example/geosamplemanager/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.example.geosamplemanager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Светлая тема (из вашей документации)
+val LightBg = Color(0xFFF5F5F5)
+val LightBgCard = Color(0xFFFFFFFF)
+val LightBgSecondary = Color(0xFFE0E0E0)
+val LightFg = Color(0xFF2C2C2C)
+val LightFgSecondary = Color(0xFF666666)
+val LightPrimary = Color(0xFF2563EB)
+
+val TagFoundLight = Color(0xFF90EE90)
+val TagBlankLight = Color(0xFFFFD700)
+val TagControlLight = Color(0xFFDDA0DD)
+val TagPostponedLight = Color(0xFFFFB6C1)
+
+// Тёмная тема
+val DarkBg = Color(0xFF1E1E1E)
+val DarkBgCard = Color(0xFF2D2D2D)
+val DarkBgSecondary = Color(0xFF3D3D3D)
+val DarkFg = Color(0xFFFFFFFF)
+val DarkFgSecondary = Color(0xFFAAAAAA)
+val DarkPrimary = Color(0xFF3B82F6)
